6eca0a35e78871caee54ac26c64817415e737379
SaahBrice <saahbrice98@gmail.com>
2023-07-10 21:57:47 +0200

    added document view and download in admin panel

--- skillWebsite/frontend/forms.py
+++ skillWebsite/frontend/forms.py
@@ -7,14 +7,42 @@ class UploadFilesForm(forms.ModelForm):
     max_file_size = 5*1024
     class Meta:
         model = DocumentPostuler
-        fields = '__all__'
+        fields = ['name','number','email','pays','ville','birthCertificate','identityDocument','recentCertificate','signedEngagement',]
+        labels = {
+            'name': 'Nom',
+            'number': 'Numéro ex.(237688888888)',
+            'email': 'Email',
+            'ville': 'Ville',
+            'pays': 'Pays',
+            'birthCertificate': 'Certificat de naissance',
+            'identityDocument': 'Document d\'identité',
+            'recentCertificate': 'Diplôme récent',
+            'signedEngagement': 'Fiche d\'enregistrement signé',
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'number': forms.NumberInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'ville': forms.TextInput(attrs={'class': 'form-control mb-4'}),
+            'pays': forms.Select(attrs={'class': 'form-control'}),
+            'birthCertificate': forms.ClearableFileInput(attrs={'class': 'form-control '}),
+            'identityDocument': forms.ClearableFileInput(attrs={'class': 'form-control '}),
+            'recentCertificate': forms.ClearableFileInput(attrs={'class': 'form-control '}),
+            'signedEngagement': forms.ClearableFileInput(attrs={'class': 'form-control mb-4'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != 'email':
+                field.required = True
     def clean(self):
         cleaned_data = super().clean()
         for field_name in self.files:
             file = cleaned_data.get(field_name)
             if file:
                 if file.size > self.max_file_size:
-                    self.add_error(field_name, f"Le fichier est trop gros (taille maximale de {self.MAX_FILE_SIZE / 1024 / 1024:.2f} Mo)" )
-                    
+                    self.add_error(field_name, f"Le fichier est trop gros (taille maximale de {self.max_file_size / 1024:.2f} Mo)" )
+
 class CaptchaTestForm(forms.Form):
-    captcha = CaptchaField()
+    captcha = CaptchaField(error_messages={'invalid': '<Your custom message>'})
--- skillWebsite/frontend/views.py
+++ skillWebsite/frontend/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CaptchaTestForm, UploadFilesForm
+from django.contrib import messages
+from .models import DocumentPostuler
 # Create your views here.
 
 
@@ -14,15 +16,31 @@ def formationNormal(request):
     return render(request, 'frontPages/meetings.html')
 
 def userHome(request):
-    return render(request, 'frontPages/user-home.html')
+    documents = DocumentPostuler.objects.all()
+    context = {
+        'documents': documents
+    }
+    return render(request, 'frontPages/user-home.html', context)
 
 def apply(request):
-    _file = UploadFilesForm
-    _captcha = CaptchaTestForm
+    _file = UploadFilesForm(prefix='file_upload')
+    _captcha = CaptchaTestForm(prefix='captcha')
     context ={
         'file':_file,
         'captcha':_captcha,
     }
+    if request.method == 'POST':
+        captcha = CaptchaTestForm(request.POST, prefix='captcha')
+        file_form = UploadFilesForm(request.POST, request.FILES, prefix='file_upload')
+        if captcha.is_valid():
+            if file_form.is_valid():
+                file_form.save()
+                messages.add_message(request, messages.SUCCESS, 'Merci! Vos document ont ete envoyer')
+                return redirect('apply')
+            else:
+                messages.add_message(request, messages.ERROR, 'Ola! Il y a un problème avec vos documents. Vérifiez que leur taille est inférieure à 5 Mo. Si le problème persiste, veuillez nous contacter. ') 
+        else:
+            messages.add_message(request, messages.ERROR, 'Ola! Reponse teste robot incorrect. SVP veillez reessayer.')
     return render(request, 'frontPages/documents.html', context)
 
 
